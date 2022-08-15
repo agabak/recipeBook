102e4d4e6e257397e53613566636dba033151b5e
Agaba Kabunga <agaba_k@hotmail.com>
2022-08-15 13:24:11 -0500

    new changes

--- src/app/recipes/services/recipe.service.ts
+++ src/app/recipes/services/recipe.service.ts
@@ -10,13 +10,15 @@ export class RecipeService {
  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test Recipe one', 'This is a test recipe', 
+    new Recipe('A test Recipe one', 
+    'This is a test recipe', 
     'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/2/27/0/0155221_Grilled-shrimp-on-skewers_s4x3.jpg.rend.hgtvcom.476.357.suffix/1397166029692.jpeg',
     [
        new Ingredient('Meat', 1),
        new Ingredient('Fries', 29)
     ]),
-    new Recipe('A test Recipe two', 'This is a test recipe Nuber two', 
+    new Recipe('A test Recipe two',
+    'This is a test recipe Nuber two', 
     'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/2/27/0/0155221_Grilled-shrimp-on-skewers_s4x3.jpg.rend.hgtvcom.476.357.suffix/1397166029692.jpeg',
     [
       new Ingredient('Burn', 2),
